ede2d85006528b9e24d2d777448a971cf1ea83ef
rstens <roland.stens@gov.bc.ca>
2025-09-07 17:19:37 -0700

    se

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ use axum::{Router, routing::{get, post}};
 use tower_http::trace::{TraceLayer, DefaultOnResponse};
 use tower_http::request_id::{MakeRequestUuid, PropagateRequestIdLayer, SetRequestIdLayer};
 use tracing::{info, Level};
-use hyper::server;
+use hyper::server::Server;
 use crate::{
     config::AppConfig,
     db::{DbState, connect_with_retry},
     routes::{health_check, create_user, get_users},
-    errors::{AppError},
+    errors,
 };
 
 #[tokio::main]
@@ -38,7 +38,7 @@ async fn main() -> Result<(), errors::AppError> {
 
     info!("🚀 API running at {}", config.server_addr);
 
-    server::bind(&config.server_addr.parse().unwrap())
+    Server::bind(&config.server_addr.parse().unwrap())
         .serve(app.into_make_service())
         .await?;
 
